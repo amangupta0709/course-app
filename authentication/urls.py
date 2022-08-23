37e02b84b8b849dc58eda31024501895bd2894ca
Aman Gupta <guptaaman704@gmail.com>
2022-08-23 12:29:12 +0530

    all git changes done

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from authentication.views import Login, Logout, Register
-
-urlpatterns = [
-    path("register/", Register.as_view(), name="register"),
-    path("login/", Login.as_view(), name="login"),
-    path("logout/", Logout.as_view(), name="logout"),
-]
